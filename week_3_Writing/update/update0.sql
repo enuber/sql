0d77a1473ad9a75243720c0c8c1752580be50039
Erik Nuber <erik.nuber@yahoo.com>
2024-04-15 16:37:03 -0700

    updating tables and cleaning data

--- week_3_Writing/update/update0.sql
+++ week_3_Writing/update/update0.sql
@@ -1,13 +1,22 @@
+-- setup once in update folder
+-- sqlite3 mfa.db
+-- .read schema.sql
+
+-- UPDATE table
+-- SET <column(s)>
+-- WHERE condition;
+
 -- Demonstrates updating authorship
 -- Uses mfa.db
 
--- Updates authorship (incorrectly)
+-- Updates authorship (incorrectly) DO NOT DO THIS
 UPDATE "created" SET "artist_id" = (
     SELECT "id" FROM "artists"
     WHERE "name" = 'Li Yin'
 );
 
 -- Updates authorship (correctly) for a piece with a previously unknown authorship
+-- this is correct way to update in created where we update the artist_id where the collection_id matches the piece we want to update. 
 UPDATE "created" SET "artist_id" = (
     SELECT "id" FROM "artists"
     WHERE "name" = 'Li Yin'
--- week_3_Writing/update/update1.sql
+++ week_3_Writing/update/update1.sql
@@ -1,16 +1,23 @@
+-- UPDATE table
+-- SET <column(s)>
+-- WHERE condition;
+
 -- Demonstrates cleaning data from a CSV of votes for favorite artwork
 -- Creates votes.db
 
--- Imports votes.csv
+-- Imports votes.csv - table of one column of titles peopel voted on.
 .import votes.csv votes
 
 -- Counts votes
+-- we will find that there are typos and extra spaces so have issues.
 SELECT "title", COUNT("title") FROM "votes" GROUP BY "title";
 
 -- Removes trailing whitespace
+-- removes leading and trailing white space.
 UPDATE "votes" SET "title" = trim("title");
 
 -- Forces to uppercase
+-- makes everything capitalized
 UPDATE "votes" SET "title" = upper("title");
 
 -- Manually updates the titles of "Farmers working at dawn"
@@ -21,6 +28,7 @@ UPDATE "votes" SET "title" = 'FARMERS WORKING AT DAWN'
 WHERE "title" = 'FAMERS WORKING AT DAWN';
 
 -- Fixes misspellings of "Farmers working at dawn"
+-- makes it easier to fix multiple mispellings, remember like is case-insensitive and % matches any pattern after. This works in small sets but be careful with large data 
 UPDATE "votes" SET "title" = 'FARMERS WORKING AT DAWN'
 WHERE "title" LIKE 'Fa%';
 
@@ -30,4 +38,4 @@ WHERE "title" LIKE 'Imag%';
 
 -- Fixes misspellings of "Profusion of flowers"
 UPDATE "votes" SET "title" = 'PROFUSION OF FLOWERS'
-WHERE "title" LIKE 'Profusion %';
+WHERE "title" LIKE 'Profusion%';
