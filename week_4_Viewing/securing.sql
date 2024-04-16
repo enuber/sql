7e81d06ea28f43f7df2805fd854c21e26a89f32c
Erik Nuber <erik.nuber@yahoo.com>
2024-04-16 11:46:00 -0700

    soft deleting views with triggers

--- week_4_Viewing/securing.sql
+++ week_4_Viewing/securing.sql
@@ -5,6 +5,7 @@
 
 -- in a ride sharing database "riders" would be considered PII as it has the name of person that took the ride. 
 
+-- ANONYMOUS -  "Anonymous" fills a column with anonymous to indicate that info is there but you don't have access to see what is there. 
 
 -- Demonstrates views for securing data
 -- Uses rideshare.db
@@ -28,14 +29,19 @@ VALUES
 SELECT * FROM "rides";
 
 -- Reveals only subset of columns
+-- Could do this to create a table that omits the riders column.
 SELECT "id", "origin", "destination" FROM "rides";
 
 -- Makes clear that rider is anonymous
+-- Anonymous leaves the riders column but fills it with "anonymous" so you don't see the names. 
 SELECT "id", "origin", "destination", 'Anonymous' AS "rider" FROM "rides";
 
 -- Creates a view
+-- Stores this in a view called analysis. 
 CREATE VIEW "analysis" AS
 SELECT "id", "origin", "destination", 'Anonymous' AS "rider" FROM "rides";
 
 -- Queries the view
 SELECT "origin", "destination", "rider" FROM "analysis";
+
+-- You can't set access controls so this data isn't very protected as still can query the "rides" table and get the rider name list if somoene really wanted to. Doing "anonymous" offers basic security as long as someone doesn't decide to just query the main table. 
--- week_4_Viewing/soft_deletion.sql
+++ week_4_Viewing/soft_deletion.sql
@@ -1,3 +1,6 @@
+-- Part 5
+-- use a deleted column to 0 or 1 to represent it's been deleted but keeping the info in the table. 
+
 -- Soft deletions
 -- mfa.db
 
@@ -11,6 +14,7 @@ SELECT * FROM "collections";
 ALTER TABLE "collections" ADD COLUMN "deleted" INTEGER DEFAULT 0;
 
 -- View data
+-- will now show the new deleted columnn.
 SELECT * FROM "collections";
 
 -- View updated schema of collections table
@@ -20,9 +24,11 @@ SELECT * FROM "collections";
 UPDATE "collections" SET "deleted" = 1 WHERE "title" = 'Farmers working at dawn';
 
 -- Select all items from collections that are not deleted
+-- removes the soft deleted column
 SELECT * FROM "collections" WHERE "deleted" = 0;
 
 -- Create a view with the same name as the collections table
+-- creating a view where we are removing the soft deleted column 
 CREATE VIEW "current_collections" AS
 SELECT "id", "title", "accession_number", "acquired" FROM "collections" WHERE "deleted" = 0;
 
@@ -30,9 +36,16 @@ SELECT "id", "title", "accession_number", "acquired" FROM "collections" WHERE "d
 SELECT * FROM "current_collections";
 
 -- Fail to delete an item from the view
+-- can't delete data because it is a view. same as why we can't update/insert.
 DELETE FROM "current_collections" WHERE "title" = 'Imaginative landscape';
 
 -- Create trigger to delete items from a view
+-- create a trigger that will allow us to do the soft delete where it's actually do this on the table we go the data from.
+
+-- INSTEAD OF - key phrase that lets us do something rather than what normally would happen.
+-- FOR EACH ROW - tells us where to run the statement between the BEGIN and END on...
+
+-- OLD."id" gives us the id of what we are referring to. The row we would have deleted so we have access to it with "OLD"
 CREATE TRIGGER "delete"
 INSTEAD OF DELETE ON "current_collections"
 FOR EACH ROW
@@ -41,6 +54,9 @@ BEGIN
 END;
 
 -- Create trigger to revert an item's deletion
+-- Trigger when we want to insert.
+-- WHEN - a keyword to check if a condition is true. in this way, we can check to see if it exists and then udpate what we need to or, if it doesn't exist we can add a whole new item as shown below.
+-- accession_number is a unique idea from the museum that references a piece of art. Different than our primary key ID.
 CREATE TRIGGER "insert_when_exists"
 INSTEAD OF INSERT ON "current_collections"
 FOR EACH ROW 
@@ -58,3 +74,6 @@ BEGIN
     INSERT INTO "collections" ("title", "accession_number", "acquired")
     VALUES (NEW."title", NEW."accession_number", NEW."acquired");
 END;
+
+INSERT INTO "current_collections" ("title", "accession_number", "acquired")
+VALUES ("Imaginative landscape", '56.496', NULL);
